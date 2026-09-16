a037feabee01f08d99721d3cdbef8b3e40a2fee4
prancismasubay-ui <prancismasubay@gmail.com>
2026-09-16 15:43:41 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 $DB_HOST = getenv('DB_HOST') ?: 'localhost';
-$DB_PORT = getenv('DB_PORT') ?: '3306';
+$DB_PORT = getenv('DB_PORT') ?: '4000';
 $DB_NAME = getenv('DB_NAME') ?: 'tdlda';
 $DB_USER = getenv('DB_USER') ?: 'root';
 $DB_PASS = getenv('DB_PASS') ?: '';
